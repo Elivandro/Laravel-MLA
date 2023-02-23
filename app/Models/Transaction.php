720dd4f6dea1c3591ab39496da6ffac1e58ba39f
Elivandro <Elivandro@hotmail.com.br>
2023-02-22 22:16:16 -0400

    relationship created

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
@@ -19,4 +20,9 @@ class Transaction extends Model
     protected $casts = [
         'status' => TransactionStatus::class
     ];
+
+    public function transaction(): BelongsTo
+    {
+        return $this->belongsTo(Transaction::class);
+    }
 }
