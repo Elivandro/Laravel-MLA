4716b0028adc7e04eb4a3a802bbd9c098b51ce7c
Elivandro <Elivandro@hotmail.com.br>
2023-03-06 21:56:14 -0400

    attributes get and set columns personalized

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -21,5 +22,27 @@ class Plan extends Model
     public function signatures(): HasMany
     {
         return $this->hasMany(Signature::class);
+    }
+
+    public function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => ucfirst($value)
+        );
+    }
+
+    public function code(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => strtoupper($value),
+            set: fn($value) => strtolower($value)
+        );
+    }
+
+    public function fullname(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value, $attributes) => $attributes['name'].' - '.$attributes['code']
+        );
     }
 }
